1092d03b6f1579876c24d2ffff860c056b567061
ManiEnayati <manii.enayati@gmail.com>
2026-05-12 17:35:01 +0330

    created first home-page using template

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('page1/', views.page1, name='page1'),
 ]
--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return render(request,"home.html")
 
 
 def page1(request):
